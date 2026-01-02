2e8633b2b9ef7d021fd8f52e0c2082f5efb2ee40
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-02 23:35:51 +0100

    none

--- common.nix
+++ common.nix
@@ -4,11 +4,7 @@
 
 { config, pkgs, ... }:
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
+  imports = [];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
