0c2a09c11900b9180a4f262414a0c8cfd4d2e41f
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-03 01:12:10 +0100

    Added nerdfont to common.nix

--- common.nix
+++ common.nix
@@ -65,6 +65,11 @@
   kitty
   ];
 
+  fonts.packages = with pkgs; [
+  nerd-fonts.jetbrains-mono
+];
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
