ab45a0a9630d92071a9f1a68a499574367542d99
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-05 00:19:25 +0100

    none

--- modules/profiles/gaming.nix
+++ modules/profiles/gaming.nix
@@ -16,9 +16,6 @@
       remotePlay.openFirewall = true;
     };
 
-    # Wine for Windows games
-    programs.wine.enable = true;
-
     # Home Manager gaming configs
     home-manager.users.nils = {
       home.packages = with pkgs; [
