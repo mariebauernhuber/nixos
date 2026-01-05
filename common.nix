d440e71ef428ddbc412cdada21eb30d95c87e97b
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-05 23:47:41 +0100

    none

--- common.nix
+++ common.nix
@@ -36,18 +36,6 @@
   i18n.inputMethod.type = "fcitx5";
   i18n.inputMethod.fcitx5.addons = [pkgs.fcitx5-mozc];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
