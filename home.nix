f3f8f10933ac1d6ec3888e25be404e8b186e47b3
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-05 20:59:59 +0100

    none

--- home.nix
+++ home.nix
@@ -26,13 +26,14 @@
 	programs.htop.enable = true;
 
 	home.packages = with pkgs; [
-	  (nwg-displays.overrideAttrs (old: {
-	    postInstall = ''
-	      wrapProgram $out/bin/nwg-displays \
-		--set XDG_CONFIG_HOME "/tmp/nwg-displays"
-	    '';
-	  }))
-	];
+  (nwg-displays.overrideAttrs (old: {
+    postInstall = ''
+      wrapProgram $out/bin/nwg-displays \
+        --set-default XDG_CONFIG_HOME "$HOME/.cache/nwg-displays"
+    '';
+  }))
+];
+
 
 	home.file.".config/hypr".source = ./dots/hypr;
 	home.file.".config/waybar".source = ./dots/waybar;
