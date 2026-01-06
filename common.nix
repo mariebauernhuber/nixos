d7d0fa9c2a57591e3e51acb9d59a1a416a761b46
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-06 23:12:30 +0100

    none

--- common.nix
+++ common.nix
@@ -17,11 +17,11 @@
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8/UTF-8";
+  i18n.defaultLocale = "ja_JP.UTF-8";
 
   i18n.extraLocales = [
-	"ja_JP.UTF-8/UTF-8"
-	"en_US.UTF-8/UTF-8"
+	"ja_JP.UTF-8"
+	"en_US.UTF-8"
 	"de_DE.UTF-8"
   ];
 
