52781db793b0ff7a69883a550d748455da0a67c6
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-06 23:11:19 +0100

    none

--- common.nix
+++ common.nix
@@ -19,9 +19,10 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "ja_JP.UTF-8/UTF-8";
 
-  i18n.supportedLocales = [
+  i18n.extraLocales = [
 	"ja_JP.UTF-8/UTF-8"
 	"en_US.UTF-8/UTF-8"
+	"de_DE.UTF-8"
   ];
 
   i18n.extraLocaleSettings = {
