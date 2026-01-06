63ecfac67c51b4a93a084de0ad35399e6a5c9db4
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-07 00:59:07 +0100

    none

--- dots/nvim/lua/custom/plugins/bufferline.lua
+++ dots/nvim/lua/custom/plugins/bufferline.lua
@@ -1,1 +1,17 @@
-return { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" }
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			separator_style = "slant",
+			show_buffer_close_icons = false,
+			-- Pin master.tex (always visible)
+			custom_filter = function(buf_number)
+				local name = vim.api.nvim_buf_get_name(buf_number)
+				return not string.match(name, "master.tex") or vim.bo[buf_number].filetype == "latex"
+			end,
+		},
+	},
+}
