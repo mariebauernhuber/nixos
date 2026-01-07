528e4087a47edb19681147fca4fb8c402e76fd9e
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-08 00:43:46 +0100

    none

--- flake.nix
+++ flake.nix
@@ -45,53 +45,53 @@
 				}
 			];
 		};
-
-		nixosConfigurations.nixos-t470s = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./common.nix
-				./hosts/t470s/hardware-configuration.nix
-				./modules/profiles/productivity.nix
-				./modules/misc.nix
-				home-manager.nixosModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.nils = import ./home.nix;
-				}
-				({ pkgs, ... }: {
-				environment.systemPackages = [
-				pkgs.prismlauncher
+		@attrs:
+		  let
+		    lib = nixpkgs.lib;
+		  in {
+			nixosConfigurations.nixos-t470s = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./common.nix
+					./hosts/t470s/hardware-configuration.nix
+					./modules/profiles/productivity.nix
+					./modules/misc.nix
+					home-manager.nixosModules.home-manager {
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+						home-manager.users.nils = import ./home.nix;
+					}
+					({ pkgs, ... }: {
+					environment.systemPackages = [
+					pkgs.prismlauncher
+					];
+									  services.fprintd = {
+					    enable = true;
+					    tod = {
+					      enable = true;
+					      driver = nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
+						calib-data-file = ./calib-data.bin;
+					      };
+					    };
+					  };
+					}
+					})
+					{
+					services.auto-cpufreq.enable = true;
+						services.auto-cpufreq.settings = {
+						  battery = {
+						     governor = "powersave";
+						     turbo = "never";
+						  };
+						  charger = {
+						     governor = "performance";
+						     turbo = "auto";
+						  };
+						};
+					}
 				];
-				@attrs:
-				  let
-				    lib = nixpkgs.lib;
-				  in {
-				  services.fprintd = {
-				    enable = true;
-				    tod = {
-				      enable = true;
-				      driver = nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
-					calib-data-file = ./calib-data.bin;
-				      };
-				    };
-				  };
-				}
-				})
-				{
-				services.auto-cpufreq.enable = true;
-					services.auto-cpufreq.settings = {
-					  battery = {
-					     governor = "powersave";
-					     turbo = "never";
-					  };
-					  charger = {
-					     governor = "performance";
-					     turbo = "auto";
-					  };
-					};
-				}
-			];
 
-		};
+			};
+		}
 	};
 }
