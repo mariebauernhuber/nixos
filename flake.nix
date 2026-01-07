b8eb14c5da5da6928e369773bd535de2b3d71d6c
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-08 00:31:19 +0100

    none

--- flake.nix
+++ flake.nix
@@ -58,6 +58,12 @@
 				environment.systemPackages = [
 				pkgs.prismlauncher
 				];
+				services.fprintd.enable = true;
+				  # If simply enabling fprintd is not enough, try enabling fprintd.tod...
+				  services.fprintd.tod.enable = true;
+				  # ...and use one of the next four drivers
+				  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+
 				})
 				{
 				services.auto-cpufreq.enable = true;
@@ -71,11 +77,6 @@
 					     turbo = "auto";
 					  };
 					};
-				services.fprintd.enable = true;
-				  # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-				  services.fprintd.tod.enable = true;
-				  # ...and use one of the next four drivers
-				  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
 				}
 			];
 
