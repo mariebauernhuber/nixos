a54ca192aefa005a725a6d3ab01595df3bdb7455
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-07 01:03:14 +0100

    Implemented bufferline into neovim config

--- dots/nvim/lua/custom/plugins/bufferline.lua
+++ dots/nvim/lua/custom/plugins/bufferline.lua
@@ -7,11 +7,6 @@ return {
 			diagnostics = "nvim_lsp",
 			separator_style = "slant",
 			show_buffer_close_icons = false,
-			-- Pin master.tex (always visible)
-			custom_filter = function(buf_number)
-				local name = vim.api.nvim_buf_get_name(buf_number)
-				return not string.match(name, "master.tex") or vim.bo[buf_number].filetype == "latex"
-			end,
 		},
 	},
 }
