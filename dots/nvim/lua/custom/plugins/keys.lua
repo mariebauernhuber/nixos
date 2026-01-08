3274b49a1b34b346c3428e9b26b263bd9321327c
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-08 20:28:37 +0100

    prev

--- dots/nvim/lua/custom/plugins/keys.lua
+++ dots/nvim/lua/custom/plugins/keys.lua
@@ -1,3 +1,11 @@
 return {
-  'tamton-aquib/keys.nvim',
+	"tamton-aquib/keys.nvim",
+	require("keys").setup({
+		enable_on_startup = false,
+		win_opts = {
+			width = 25,
+			-- etc
+		},
+		-- TODO: more options later
+	}),
 }
