51e786eed31efa97983d835675bff121e761a48c
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-09 13:30:00 +0100

    prev

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,6 +1,11 @@
 { config, pkgs, lib, ... }:
 
-{
+let
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nixvim";
+    # When using a different channel you can use `ref = "nixos-<version>"` to set it here
+  });
+in{
     # Productivity packages
     environment.systemPackages = with pkgs; [
 	unzip # DEP for neovim (stylua)
